eac60d3abd2618e9b5010dfffb59adb7b6d4c213
Srujana Kovuri <135617161+ksrujana04@users.noreply.github.com>
2025-03-02 22:18:17 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,81 @@
+var express = require("express");
+var path = require("path");
+var bodyParser = require("body-parser");
+var compiler = require("compilex");
+
+
+var app = express();
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser());
+
+var option = { stats: true };
+compiler.init(option);
+app.get("/", function (req, res) {
+    res.sendfile(__dirname + "/index.html");
+});
+
+app.post("/compilecode", function (req, res) {
+    var code = req.body.code;
+    var input = req.body.input;
+    var inputRadio = req.body.inputRadio;
+    var lang = req.body.lang;
+
+    if (lang === "C" || lang === "C++") {
+        if (inputRadio === "true") {
+            var envData = { OS: "windows", cmd: "g++", options: { timeout: 10000 } };
+            compiler.compileCPPWithInput(envData, code, input, function (data) {
+                if (data.error) {
+                    res.send(data.error);
+                } else {
+                    res.send(data.output);
+                }
+            });
+        } else {
+            var envData = { OS: "windows", cmd: "g++", options: { timeout: 10000 } };
+            compiler.compileCPP(envData, code, function (data) {
+                res.send(data);
+                //data.error = error message
+                //data.output = output value
+            });
+        }
+    }
+    if (lang === "Python") {
+        if (inputRadio === "true") {
+            var envData = { OS: "windows" };
+            compiler.compilePythonWithInput(envData, code, input, function (data) {
+                res.send(data);
+            });
+        } else {
+            var envData = { OS: "windows" };
+            compiler.compilePython(envData, code, function (data) {
+                res.send(data);
+            });
+        }
+    }
+    
+    if (lang === "Java") {
+        var envData = { OS: "windows" };
+        if (inputRadio === "true") {
+            compiler.compileJavaWithInput(envData, code, input, function (data) {
+                res.send(data);
+            });
+        } else {
+            compiler.compileJava(envData, code, function (data) {
+                res.send(data);
+            });
+        }
+    }
+});
+
+app.get("/fullStat", function (req, res) {
+    compiler.fullStat(function (data) {
+        res.send(data);
+    });
+});
+
+app.listen(8080);
+
+compiler.flush(function () {
+    console.log("All temporary files flushed !");
+});
